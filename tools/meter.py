8d472602a764bc092124e9bc11a823b7db570844
harigro <trierbank@gmail.com>
2024-12-12 19:18:11 +0700

    menambahkan validasi string yang berisi bilangan

--- tools/meter.py
+++ tools/meter.py
@@ -1,8 +1,19 @@
 from typing import Union, Dict
+import re
 
+class ValidateString:
+    def __init__(self, value: str):
+        self.value = value
+
+    def is_int(self) -> bool:
+        return re.fullmatch(r"-?\d+", self.value) is not None
+
+    def is_float(self) -> bool:
+        return re.fullmatch(r"-?\d+\.\d+", self.value) is not None
+    
 class TabelSatuanMeter:
     """Membuat tabel konversi untuk satuan meter"""
-    def __init__(self, angka: Union[float, int], satuan: str):
+    def __init__(self, angka: str, satuan: str):
         self.angka = angka
         self.satuan = satuan
         self.data = {
@@ -15,11 +26,18 @@ class TabelSatuanMeter:
             "mm": pow(10, 6)
         }
 
+    def validation_string(self) -> Union[int, float]:
+        vv = ValidateString(self.angka)
+        if vv.is_float():
+            return float(self.angka)
+        if vv.is_int():
+            return int(self.angka)
+
     def tabel_new(self) -> Dict[str, str]:
         """menghasilkan tabel baru"""
         convert_to_km = self.data.get(self.satuan)
-        return {k: str(v * convert_to_km * self.angka) for k, v in self.data.items()}
-    
-if __name__=="__main__":  
-    tt = TabelSatuanMeter(1.25, "cm").tabel_new()
+        return {k: str(v * convert_to_km * self.validation_string()) for k, v in self.data.items()}
+
+if __name__=="__main__":
+    tt = TabelSatuanMeter("1.25", "cm").tabel_new()
     print(tt)
--- tools/tests/test_meter.py
+++ tools/tests/test_meter.py
@@ -1,16 +1,16 @@
 import unittest
-from ..meter import TabelSatuanMeter
+from ..meter import TabelSatuanMeter, ValidateString
 
 class TestTabelSatuanMeter(unittest.TestCase):
 
     def test_kunci_tabel_meter(self):
-        tt = TabelSatuanMeter(2.5, "m").data
+        tt = TabelSatuanMeter("2.5", "m").data
         kunci = [k for k in tt.keys()]
         data = ["km", "hm", "dam", "m", "dm", "cm", "mm"]
         self.assertEqual(kunci, data)
 
     def test_konversi_m(self):
-        tt = TabelSatuanMeter(2.5, "m").tabel_new()
+        tt = TabelSatuanMeter("2.5", "m").tabel_new()
         expected = {
             'km': '2500.0',
             'hm': '25000.0',
@@ -24,7 +24,25 @@ class TestTabelSatuanMeter(unittest.TestCase):
 
     def test_satuan_tidak_valid(self):
         with self.assertRaises(TypeError):
-            TabelSatuanMeter(3.5, "invalid").tabel_new()
+            TabelSatuanMeter("3.5", "invalid").tabel_new()
+
+class TestValidateString(unittest.TestCase):
+
+    def test_validasi_string_int(self):
+        data = ValidateString("12").is_int()
+        self.assertEqual(True, data)
+
+    def test_validasi_string_float(self):
+        data = ValidateString("1.2").is_float()
+        self.assertEqual(True, data)
+
+    def test_validasi_string_int_adalah_False(self):
+        data = ValidateString("1.2").is_int()
+        self.assertEqual(False, data)
+
+    def test_validasi_string_float_adalah_False(self):
+        data = ValidateString("12").is_float()
+        self.assertEqual(False, data)
 
 if __name__ == "__main__":
     unittest.main()
