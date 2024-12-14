60b2b318ac450302a9a1477a0bb9ad4cef63e485
harigro <trierbank@gmail.com>
2024-12-14 20:03:24 +0700

    menambahkan modal peringatan untuk kesalahan inputan

--- static/script.js
+++ static/script.js
@@ -1,7 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // ambil kelas modal
+  const modal = document.querySelector(".modal");
+  const overlay = document.querySelector(".overlay");
+  const btnCloseModal = document.querySelector(".close-modal");
+  const closeModal = () => {
+        modal.classList.add("hidden");
+        overlay.classList.add("hidden");
+      };
+
+  btnCloseModal.addEventListener("click", closeModal);
+  overlay.addEventListener("click", closeModal);
+  const buat_modal = () => {
+    modal.classList.remove("hidden");
+      overlay.classList.remove("hidden");
+
+      
+  }
   // ambil tombol tekan berdasarkan kelas
   const tombolMeter = document.querySelector(".tombol");
-
   tombolMeter.addEventListener("click", () => {
     const input_pengguna = document.querySelector(".angka");
     const nilai_input = document.querySelectorAll(".result");
@@ -10,15 +26,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (match) {
       // Mengirim permintaan ke server menggunakan fetch()
-      fetch('/greet', {
-        method: 'POST',
+      fetch("/greet", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
+          "Content-Type": "application/x-www-form-urlencoded",
         },
-        body: `nilai=${encodeURIComponent(match[1])}&satuan=${encodeURIComponent(match[2])}`
+        body: `nilai=${encodeURIComponent(
+          match[1]
+        )}&satuan=${encodeURIComponent(match[2])}`,
       })
-      .then(response => response.json())  // Mengubah response menjadi format JSON
-      .then(data => {
+        .then((response) => response.json()) // Mengubah response menjadi format JSON
+        .then((data) => {
           // Menampilkan hasil ke dalam input
           input_pengguna.value = data.greeting.m;
           // nilai_input.forEach((n, item) => {
@@ -28,13 +46,13 @@ document.addEventListener("DOMContentLoaded", () => {
           //     Math.pow(10, item);
           //   n.value = cocok(nilai.toFixed(7-item-1)) + " " + n.classList[0];
           // });
-      })
-      .catch(error => {
+        })
+        .catch((error) => {
           // Menampilkan error jika terjadi kesalahan
-          resultDiv.innerHTML = '<p>Terjadi kesalahan!</p>';
-      });
+          resultDiv.innerHTML = "<p>Terjadi kesalahan!</p>";
+        });
     } else {
-      alert("Input tidak valid!");
+      buat_modal()
     }
   });
 });
