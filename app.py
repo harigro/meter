93e5108ee5fd340589420ba44522ffaed924a93a
harigro <trierbank@gmail.com>
2024-12-30 09:25:59 +0700

    menambahkan modul tools untuk mempermudah perhitungan

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template
+from tools.meter import TabelSatuanMeter
 
 app = Flask(__name__)
 
@@ -10,13 +11,9 @@ def index():
 def greet():
     # nilai kembalian {'nilai': '12', 'satuan': 'cm'}
     meter = request.form.to_dict()
-    print(meter)
+    tabel = TabelSatuanMeter(meter.get("nilai"), meter.get("satuan"))
     if meter:
-        greeting = {'km': '1200000', 'hm': '12000000', 'dam': '120000000', 
-                    'm': '1200000000', 'dm': '12000000000', 'cm': '120000000000', 'mm': '1200000000000'}
-    else:
-        greeting = "Halo, siapa namamu?"
-    
+        greeting = tabel.tabel_new()
     return jsonify(greeting=greeting)
 
 if __name__ == '__main__':
